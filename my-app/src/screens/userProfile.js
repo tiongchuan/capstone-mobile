b7e74dc9fdf669c0ca871a2924af596fd987c360
MiaKim0804 <kimmichino0804@gmail.com>
2022-08-31 14:53:28 +0800

    add:image picker pass:tuor profile information to request tutor page

--- my-app/src/screens/userProfile.js
+++ my-app/src/screens/userProfile.js
@@ -1,41 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { 
   View, 
   Text, 
-  TouchableOpacity 
+  Image,
+  TouchableOpacity
 } from 'react-native'
+import * as ImagePicker from 'expo-image-picker'
 import styles from '../styles/userProfile.styles'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 export const UserProfileScreen = ({ navigation, route }) => {
+  const [image, setImage] = useState(null)
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  }
+
   return (
 
     <View style = { styles.container }>
-      <View style = { styles.imgContainer }>
-        <MaterialCommunityIcons 
-          style = { styles.img } 
-          name="camera-plus" size={120} 
-          color="#A7C7E7" />
+      <View  style = { styles.imgContainer }>
+        <TouchableOpacity style = { styles.btn } onPress = { pickImage } >
+          <MaterialCommunityIcons 
+            style = { styles.img } 
+            name = "camera-plus" size = { 120 } 
+            color = "#A7C7E7" />
+            { image && <Image source = {{ uri: image }} style = { styles.profileImg }/>}
+        </TouchableOpacity>
       </View>
       <View style = { styles.textContainer }>  
-        <TouchableOpacity 
-          onPress = {() => navigation.navigate('Account')}>
+        <TouchableOpacity onPress = {() => navigation.navigate('Account')}>
           <View style = { styles.arrow }>
             <View style = { styles.icon }>
-              <MaterialCommunityIcons name="account-circle-outline" size={30} color="#A7C7E7" />
-              <Text style={styles.text}>Account</Text>
+              <MaterialCommunityIcons name = "account-circle-outline" size = { 30 } color = "#A7C7E7" />
+              <Text style = { styles.text }>Account</Text>
             </View>
-            <MaterialCommunityIcons name="chevron-right" size={29} color="#D9D9D9" />
+            <MaterialCommunityIcons name = "chevron-right" size = { 29 } color = "#D9D9D9" />
           </View>
         </TouchableOpacity> 
-        <TouchableOpacity 
-          onPress = {() => navigation.navigate('My Activity')}>
+        <TouchableOpacity onPress = {() => navigation.navigate('My Activity')}>
           <View style = { styles.arrow }>
             <View style = { styles.icon }>
-              <MaterialCommunityIcons name="bookmark-outline" size={30} color="#A7C7E7" />
-              <Text style={styles.text}>Current Bookings</Text>
+              <MaterialCommunityIcons name = "bookmark-outline" size = { 30 } color = "#A7C7E7" />
+              <Text style = { styles.text }>Current Bookings</Text>
             </View>
-            <MaterialCommunityIcons name="chevron-right" size={29} color="#D9D9D9" />
+            <MaterialCommunityIcons name = "chevron-right" size = { 29 } color = "#D9D9D9" />
           </View>
         </TouchableOpacity>
         <TouchableOpacity 
@@ -43,10 +63,10 @@ export const UserProfileScreen = ({ navigation, route }) => {
         >
           <View style = { styles.arrow }>
             <View style = { styles.icon }>
-              <MaterialCommunityIcons name="email-outline" size={30} color="#A7C7E7" />
-              <Text style={styles.text}>Contact Us</Text>
+              <MaterialCommunityIcons name = "email-outline" size = { 30 } color = "#A7C7E7" />
+              <Text style = { styles.text }>Contact Us</Text>
             </View>
-            <MaterialCommunityIcons name="chevron-right" size={29} color="#D9D9D9" />
+            <MaterialCommunityIcons name = "chevron-right" size = { 29 } color = "#D9D9D9" />
           </View>
         </TouchableOpacity>
         <TouchableOpacity 
@@ -54,20 +74,19 @@ export const UserProfileScreen = ({ navigation, route }) => {
         >
           <View style = { styles.arrow }>
             <View style = { styles.icon }>
-              <MaterialCommunityIcons name="cog-outline" size={30} color="#A7C7E7" />
-              <Text style={styles.text}>Settings</Text>
+              <MaterialCommunityIcons name = "cog-outline" size = { 30 } color = "#A7C7E7" />
+              <Text style = { styles.text }>Settings</Text>
             </View>
-            <MaterialCommunityIcons name="chevron-right" size={29} color="#D9D9D9" />
+            <MaterialCommunityIcons name = "chevron-right" size = { 29 } color = "#D9D9D9" />
           </View>
         </TouchableOpacity>
-        <TouchableOpacity 
-          onPress = {() => navigation.navigate('Home')}>
+        <TouchableOpacity onPress = {() => navigation.navigate('Home')}>
           <View style = { styles.arrow }>
             <View style = { styles.icon }>
-              <MaterialCommunityIcons name="exit-to-app" size={30} color="#A7C7E7" />
-              <Text style={styles.text}>Log out</Text>
+              <MaterialCommunityIcons name = "exit-to-app" size = { 30 } color = "#A7C7E7" />
+              <Text style = { styles.text }>Log out</Text>
             </View>
-            <MaterialCommunityIcons name="chevron-right" size={29} color="#D9D9D9" />
+            <MaterialCommunityIcons name = "chevron-right" size = { 29 } color = "#D9D9D9" />
           </View>
         </TouchableOpacity>
       </View>
